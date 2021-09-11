ba5086922a61da34f375c72515e92372ae5a1626
zerkuru <zikonnikova@gmail.com>
2021-09-11 17:03:49 +0300

    Домашнее задание 7
    разделы 1, 2, 3, без 4

---
+++ 7/3 уровень/1-tabs/app.js
@@ -0,0 +1,61 @@
+'use strict';
+const texts = {
+    text1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    text2: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.',
+    text3: 'Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил.'
+};
+
+/* 
+1. Получите ссылку на .text, например с помощью querySelector
+2. Получите коллекцию, в которой хранятся все .nav-link, например с помощью querySelectorAll
+    2.1 Переберите полученную коллекцию, например с помощью forEach, и каждой ссылке назначьте
+    обработчик клика функцию clickHandler.
+*/
+textEl = document.querySelector('.text');
+navEls = document.querySelectorAll('.nav-link');
+navEls.forEach(function (navEl) {
+    buttonEl.addEventListener('click', clickHandler(event));
+});
+
+/**
+ * Обработчик клика по .nav-link
+ * @param {MouseEvent} event 
+ */
+function clickHandler(event) {
+    // здесь вызывайте changeText и changeActiveClass, и передавайте
+    // им объект события.
+    changeText(event);
+    changeActiveClass(event);
+
+};
+
+/**
+ * Эта функция должна убирать .active у предыдущего .nav-link и ставить его
+ * на тот, по которому кликнули.
+ * @param {MouseEvent} event 
+ */
+function changeActiveClass(event) {
+    let actEl = document.querySelector('.active');
+    actEl.classList.remove('active');
+    event.target.classList.add('active');
+};
+
+/**
+ * Эта фукнция должна читать текст (например через textContent) из 
+ * .nav-link по которому кликнули и в зависимости от этого в .text
+ * ставить соответствующий текст из texts.
+ * @param {MouseEvent} event 
+ */
+function changeText(event) {
+    switch (event.target.textContent) {
+        case "Link 1":
+            textEl.textContent = texts.text1;
+            break;
+        case "Link 2":
+            textEl.textContent = texts.text2;
+            break;
+        case "Link 3":
+            textEl.textContent = texts.text3;
+            break;
+    };
+};
